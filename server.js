90fca79ae9bfa482668ac10b44f3af26c48f6df1
enkhjinpurevjav <enkhjinp@gmail.com>
2025-10-27 10:11:37 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 import express from "express"; // or const express = require("express");
 const app = express();
 
-app.get("/health", (req, res) => {
-  res.json({ status: "ok" });
+app.get("/", (_req, res) => {
+  res.json({ name: "M Dent API", status: "ok" }); // (API—программын интерфэйс)
 });
 
 const port = process.env.PORT || 80;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+
+
+
+
