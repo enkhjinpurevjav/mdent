865d002fc5fd3573ff1b8127d987d86dc6776791
enkhjinpurevjav <enkhjinp@gmail.com>
2025-10-28 08:18:37 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 // server.js (CommonJS)
+require('dotenv').config();
+
 const express = require('express');
 const { PrismaClient } = require('@prisma/client');
 const jwt = require('jsonwebtoken');
